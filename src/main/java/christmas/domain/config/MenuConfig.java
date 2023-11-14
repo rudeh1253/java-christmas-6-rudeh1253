4051d214d21fbd3b3dd5545987ea89ebb9cceb83
pgd <rudeh1253@gmail.com>
2023-11-14 16:44:38 +0900

    chore: 새로운 설정 추가

--- src/main/java/christmas/domain/config/MenuConfig.java
+++ src/main/java/christmas/domain/config/MenuConfig.java
@@ -4,4 +4,5 @@ public class MenuConfig {
     public static final String MENU_INPUT_REGEX = "^[ㄱ-ㅎ가-힣]+-[0-9]+$";
     public static final String MENU_DELIMITER = ",";
     public static final String MENU_AND_NUMBER_DELIMITER = "-";
+    public static final int MIN_ORDER_QUANTITY_OF_ONE_MENU = 1;
 }
