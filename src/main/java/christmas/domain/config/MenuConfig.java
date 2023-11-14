792cd03644d3139c99b473d07a8b89bbdf9a46af
pgd <rudeh1253@gmail.com>
2023-11-14 16:40:13 +0900

    feat: 주문할 메뉴와 개수 입력 구현

--- src/main/java/christmas/domain/config/MenuConfig.java
+++ src/main/java/christmas/domain/config/MenuConfig.java
@@ -3,4 +3,5 @@ package christmas.domain.config;
 public class MenuConfig {
     public static final String MENU_INPUT_REGEX = "^[ㄱ-ㅎ가-힣]+-[0-9]+$";
     public static final String MENU_DELIMITER = ",";
+    public static final String MENU_AND_NUMBER_DELIMITER = "-";
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,7 +1,11 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.config.MenuConfig;
 import christmas.validation.InputValidator;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class InputView {
     private static final InputView SINGLETON = new InputView();
@@ -20,5 +24,17 @@ public class InputView {
         String input = Console.readLine();
         INPUT_VALIDATOR.validateDate(input);
         return Integer.parseInt(input);
+    }
+
+    public Map<String, Integer> readMenu() {
+        OUTPUT_VIEW.askMenuAndNumber();
+        String input = Console.readLine();
+        String[] parsedInput = input.split(MenuConfig.MENU_DELIMITER);
+        Map<String, Integer> orders = new HashMap<>();
+        for (String each : parsedInput) {
+            String[] menuAndNumber = each.split(MenuConfig.MENU_AND_NUMBER_DELIMITER);
+            orders.put(menuAndNumber[0], Integer.parseInt(menuAndNumber[1]));
+        }
+        return orders;
     }
 }
