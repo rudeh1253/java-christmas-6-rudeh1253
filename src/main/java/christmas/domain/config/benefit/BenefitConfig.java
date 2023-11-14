5f9a12d70dbcf63c8d80713d3b0019afeabd86e1
pgd <rudeh1253@gmail.com>
2023-11-15 01:14:24 +0900

    refactor: Discount에 정의된 상수를 BenefitConfig로 이동

--- src/main/java/christmas/domain/config/benefit/BenefitConfig.java
+++ src/main/java/christmas/domain/config/benefit/BenefitConfig.java
@@ -4,4 +4,5 @@ import christmas.domain.config.Menu;
 
 public class BenefitConfig {
     public static final Menu GIVEAWAY_ITEM = Menu.CHAMPAGNE;
+    public static final int STANDARD_FOR_GIVEAWAY = 120000;
 }
--- src/main/java/christmas/domain/config/benefit/DiscountConfig.java
+++ src/main/java/christmas/domain/config/benefit/DiscountConfig.java
@@ -3,7 +3,6 @@ package christmas.domain.config.benefit;
 import christmas.domain.config.MenuClassification;
 
 public class DiscountConfig {
-    public static final int STANDARD_FOR_GIVEAWAY = 120000;
     public static final int MIN_ORDER_AMOUNT_FOR_DISCOUNT = 10000;
     public static final int DISCOUNT_FOR_SPECIAL_DAY = 1000;
     public static final int SPECIAL_DISCOUNT_PER_MENU = 2023;
