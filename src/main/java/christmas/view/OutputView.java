3442e0ba8e41356ee292122cb60b50927d9031fd
pgd <rudeh1253@gmail.com>
2023-11-14 10:41:33 +0900

    feat: 출력을 위한 view 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,31 @@
+package christmas.view;
+
+import static christmas.view.config.OutputConfig.*;
+
+public class OutputView {
+
+    private void printWithLine(String msg) {
+        System.out.println(msg);
+    }
+
+    private void printBlankLine() {
+        System.out.println();
+    }
+
+    public void printStartMessage() {
+        printWithLine(PROGRAM_START_MESSAGE.get());
+    }
+
+    public void askVisitDate() {
+        printWithLine(ASK_VISIT_DATE.get());
+    }
+
+    public void askMenuAndNumber() {
+        printWithLine(ASK_MENU_AND_NUMBER.get());
+    }
+
+    public void printResultTitle() {
+        printWithLine(RESULT_TITLE.get());
+        printBlankLine();
+    }
+}
---
+++ src/main/java/christmas/view/config/OutputConfig.java
@@ -0,0 +1,28 @@
+package christmas.view.config;
+
+public enum OutputConfig {
+    PROGRAM_START_MESSAGE("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
+    ASK_VISIT_DATE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
+    ASK_MENU_AND_NUMBER("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
+    RESULT_TITLE("12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
+    ORDER_MENU("<주문 메뉴>"),
+    PRESENT_LIST("<혜택 내역>"),
+    CHRISTMAS_D_DAY_DISCOUNT("크리스마스 디데이 할인"),
+    WEEKDAY_DISCOUNT("평일 할인"),
+    WEEKEND_DISCOUNT("주말 할인"),
+    SPECIAL_DISCOUNT("특별 할인"),
+    GIFT_EVENT("증정 이벤트"),
+    TOTAL_DISCOUNT("<총혜택 금액>"),
+    EXPECTED_TOTAL_COST("<할인 후 예상 결제 금액>"),
+    DECEMBER_EVENT_BADGE("<12월 이벤트 배지>");
+
+    private String message;
+
+    private OutputConfig(String message) {
+        this.message = message;
+    }
+
+    public String get() {
+        return this.message;
+    }
+}
