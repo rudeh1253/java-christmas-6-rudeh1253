7effb19cebe55d0095ec6ac5ab03d47763dd1d29
pgd <rudeh1253@gmail.com>
2023-11-14 19:16:58 +0900

    chore(DateConfig): enum을 일반 클래스로 변경

--- src/main/java/christmas/domain/config/DateConfig.java
+++ src/main/java/christmas/domain/config/DateConfig.java
@@ -1,16 +1,6 @@
 package christmas.domain.config;
 
-public enum DateConfig {
-    DATE_MIN(1),
-    DATE_MAX(31);
-
-    private final int date;
-
-    DateConfig(int date) {
-        this.date = date;
-    }
-
-    public int get() {
-        return this.date;
-    }
+public class DateConfig {
+    public static final int DATE_MIN = 1;
+    public static final int DATE_MAX = 31;
 }
